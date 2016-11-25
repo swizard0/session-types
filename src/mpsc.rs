fdb56978a24db2cb5a22643a513f3c60155942f5
Alexey Voznyuk <me@swizard.info>
2016-11-25 03:09:40 +0300

    small renaming

--- src/mpsc.rs
+++ src/mpsc.rs
@@ -9,15 +9,15 @@ use std::collections::HashMap;
 
 use super::{ChannelSend, ChannelRecv, Carrier, HasDual, Chan};
 
-pub struct ChannelMPSC {
+pub struct Channel {
     tx: Sender<Box<u8>>,
     rx: Receiver<Box<u8>>,
 }
 
-pub struct ValueMPSC<T>(pub T) where T: Send + 'static;
+pub struct Value<T>(pub T) where T: Send + 'static;
 
-impl<T> ChannelSend for ValueMPSC<T> where T: Send + 'static {
-    type Crr = ChannelMPSC;
+impl<T> ChannelSend for Value<T> where T: Send + 'static {
+    type Crr = Channel;
     type Err = SendError<Box<T>>;
 
     fn send(self, carrier: &mut Self::Crr) -> Result<(), Self::Err> {
@@ -28,41 +28,41 @@ impl<T> ChannelSend for ValueMPSC<T> where T: Send + 'static {
     }
 }
 
-impl<T> ChannelRecv for ValueMPSC<T> where T: Sized + Send + 'static {
-    type Crr = ChannelMPSC;
+impl<T> ChannelRecv for Value<T> where T: Sized + Send + 'static {
+    type Crr = Channel;
     type Err = RecvError;
 
     fn recv(carrier: &mut Self::Crr) -> Result<Self, Self::Err> {
         unsafe {
             let rx: &Receiver<Box<T>> = transmute(&carrier.rx);
-            rx.recv().map(|v| ValueMPSC(*v))
+            rx.recv().map(|v| Value(*v))
         }
     }
 }
 
-impl Carrier for ChannelMPSC {
+impl Carrier for Channel {
     type SendChoiceErr = SendError<Box<bool>>;
     fn send_choice(&mut self, choice: bool) -> Result<(), Self::SendChoiceErr> {
-        ValueMPSC(choice).send(self)
+        Value(choice).send(self)
     }
 
     type RecvChoiceErr = RecvError;
     fn recv_choice(&mut self) -> Result<bool, Self::RecvChoiceErr> {
-        ValueMPSC::recv(self).map(|ValueMPSC(value)| value)
+        Value::recv(self).map(|Value(value)| value)
     }
 }
 
 /// Returns two session channels
 #[must_use]
-pub fn session_channel<P: HasDual>() -> (Chan<ChannelMPSC, (), P>, Chan<ChannelMPSC, (), P::Dual>) {
+pub fn session_channel<P: HasDual>() -> (Chan<Channel, (), P>, Chan<Channel, (), P::Dual>) {
     let (master_tx, slave_rx) = channel();
     let (slave_tx, master_rx) = channel();
 
-    let master_carrier = ChannelMPSC {
+    let master_carrier = Channel {
         tx: master_tx,
         rx: master_rx,
     };
-    let slave_carrier = ChannelMPSC {
+    let slave_carrier = Channel {
         tx: slave_tx,
         rx: slave_rx,
     };
@@ -73,8 +73,8 @@ pub fn session_channel<P: HasDual>() -> (Chan<ChannelMPSC, (), P>, Chan<ChannelM
 
 /// Connect two functions using a session typed channel.
 pub fn connect<FM, FS, P>(master_fn: FM, slave_fn: FS) where
-    FM: Fn(Chan<ChannelMPSC, (), P>) + Send,
-    FS: Fn(Chan<ChannelMPSC, (), P::Dual>) + Send + 'static,
+    FM: Fn(Chan<Channel, (), P>) + Send,
+    FS: Fn(Chan<Channel, (), P::Dual>) + Send + 'static,
     P: HasDual + Send + 'static,
     <P as HasDual>::Dual: HasDual + Send + 'static
 {
