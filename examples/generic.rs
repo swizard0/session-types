11e7aa55faffd1fb505f8e894e592822201d0dcb
Alexey Voznyuk <me@swizard.info>
2016-11-27 02:27:13 +0300

    port `generic.rs` example

--- examples/generic.rs
+++ examples/generic.rs
@@ -2,25 +2,23 @@
 ///
 /// This example demonstrates how we can use traits to send values through a
 /// channel without actually knowing the type of the value.
-
 extern crate session_types_ng;
+
 use session_types_ng::*;
 
-use std::thread::spawn;
-
-fn srv<A: std::marker::Send+'static>(x: A, c: Chan<(), Send<A, Eps>>) {
-    c.send(x).close();
+fn srv<A>(x: A, c: Chan<mpsc::Channel, (), Send<mpsc::Value<A>, End>>) where A: std::marker::Send + 'static {
+    c.send(mpsc::Value(x)).unwrap().close();
 }
 
-fn cli<A: std::marker::Send+std::fmt::Debug+'static>(c: Chan<(), Recv<A, Eps>>) {
-    let (c, x) = c.recv();
+fn cli<A>(c: Chan<mpsc::Channel, (), Recv<mpsc::Value<A>, End>>) where A: std::marker::Send + std::fmt::Debug + 'static {
+    let (c, mpsc::Value(x)) = c.recv().unwrap();
     println!("{:?}", x);
     c.close();
 }
 
 fn main() {
-    let (c1, c2) = session_channel();
-    let t = spawn(move || srv(42u8, c1));
+    let (c1, c2) = mpsc::session_channel();
+    let t = std::thread::spawn(move || srv(42u8, c1));
     cli(c2);
 
     t.join().unwrap();
